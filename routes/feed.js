c1d23d35d5b43101490e8cf5fcaecbffb45741bd
FellipeMiguel <fellioemiguel@gmail.com>
2025-04-21 15:59:32 -0300

    first commit

---
+++ routes/feed.js
@@ -0,0 +1,24 @@
+const expreess = require("express");
+const { body } = require("express-validator");
+
+const feedController = require("../controllers/feed");
+
+const router = expreess.Router();
+
+// GET /feed/posts
+router.get("/posts", feedController.getPosts);
+// POST /feed/post
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+// GET /feed/post/:postId
+router.get("/post/:postId", feedController.getPost);
+// PUT /feed/post/:postId
+router.put("/post/:postId");
+
+module.exports = router;
