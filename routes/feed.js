de863e5d7c50aead0b0322fbecdb7a5b01bfaffc
FellipeMiguel <fellioemiguel@gmail.com>
2025-04-21 17:31:07 -0300

    feat: Adding a User Model

--- routes/feed.js
+++ routes/feed.js
@@ -19,6 +19,15 @@ router.post(
 // GET /feed/post/:postId
 router.get("/post/:postId", feedController.getPost);
 // PUT /feed/post/:postId
-router.put("/post/:postId");
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete("/post/:postId", feedController.deletePost);
 
 module.exports = router;
